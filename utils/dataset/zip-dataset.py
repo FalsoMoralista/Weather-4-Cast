f6fbdcc48e5bb594385bd0e504b1ff7bf6d8723c
Almir Neto <almirneto338@gmail.com>
2025-08-17 12:55:50 -0300

    adding a function to zip dataset files in the same zip unless there is a file larger than the max defined size

--- utils/dataset/zip-dataset.py
+++ utils/dataset/zip-dataset.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from pathlib import Path
-import tarfile
+import zipfile
 
 dataset_name = sys.argv[1]
 
@@ -16,44 +16,43 @@ def zip_dataset(dataset_name):
     if not pack_path.exists():
         pack_path.mkdir(parents=True)
 
+    files = dataset_path.glob("**/*")
+    files_name = [file for file in files if file.is_file()]
+    files_name = sorted(files_name, key=lambda x: str(x.relative_to(dataset_path)))
 
-files = Path("./dataset/w4c24").glob("**/*")
-total_size = 0
-pack_idx = 0
-tar_filename = f"packs/w4c24-{pack_idx}.tar"
+    MAX_SIZE = 5 * 1024 * 1024 * 1024
 
-tarf = tarfile.TarFile(tar_filename, "a")
+    pack_idx = 0
 
+    current_pack_size = 0
+    current_pack_zip = zipfile.ZipFile(
+        pack_path / f"{dataset_name}_{pack_idx}.zip", "w", zipfile.ZIP_DEFLATED
+    )
 
-gb_2 = 2 * 1024 * 1024 * 1024  # 2GB in bytes
+    for file in files:
+        file_size = os.path.getsize(file)
+        if file_size > MAX_SIZE:
+            pack_idx += 1
+            large_file_zip = zipfile.ZipFile(
+                pack_path / f"{dataset_name}_{pack_idx}.zip", "w", zipfile.ZIP_DEFLATED
+            )
+            large_file_zip.write(file, arcname=file.relative_to(dataset_path))
+            large_file_zip.close()
+            print(
+                f"File {file} is larger than 5GB, packed separately. Pack index: {pack_idx}"
+            )
+            continue
+        if current_pack_size + file_size > MAX_SIZE:
+            current_pack_zip.close()
+            pack_idx += 1
+            current_pack_zip = zipfile.ZipFile(
+                pack_path / f"{dataset_name}_{pack_idx}.zip", "w", zipfile.ZIP_DEFLATED
+            )
+            current_pack_size = 0
+            print(f"Pack {pack_idx} reached 5GB, starting a new pack.")
+        current_pack_zip.write(file, arcname=file.relative_to(dataset_path))
+        current_pack_size += file_size
 
-for file in files:
-    if file.is_dir():
-        continue
-    file_size = os.path.getsize(file)
-    if file_size > gb_2:
-        if total_size > 0:
-            tarf.close()
-            total_size = 0
-        pack_idx += 1
-        tar_filename = f"packs/w4c24-{pack_idx}.tar"
-        tarf = tarfile.TarFile(tar_filename, "a")
-        tarf.add(file, arcname=file.relative_to("./dataset/w4c24"), recursive=False)
-        tarf.close()
-        print(f"File {file} is larger than 2GB, packed separately.")
-        pack_idx += 1
-        tar_filename = f"packs/w4c24-{pack_idx}.tar"
-        tarf = tarfile.TarFile(tar_filename, "a")
-        continue
-    if total_size + file_size > gb_2:
-        if total_size > 0:
-            total_size = 0
-            tarf.close()
-        print(f"Pack {pack_idx} reached 2GB, starting a new pack.")
-        total_size = 0
-        pack_idx += 1
-        tar_filename = f"packs/w4c24-{pack_idx}.tar"
-        tarf = tarfile.TarFile(tar_filename, "a")
-    tarf.add(file, arcname=file.relative_to("./dataset/w4c24"), recursive=False)
-    total_size += file_size
-tarf.close()
+    if current_pack_size > 0:
+        current_pack_zip.close()
+        print(f"Final pack {pack_idx}")
