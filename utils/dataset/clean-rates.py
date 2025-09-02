e8628da350251b2681f835740e0cbbdd911d3f91
FalsoMoralista <lucianoadfilho@gmail.com>
2025-09-02 08:31:17 -0300

    adding opera file cleaning

---
+++ utils/dataset/clean-rates.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+import torch
+import h5py
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class OperaCleaning:
+    KEY = "rates.crop"
+
+    def __init__(self, path: str):
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The file {path} does not exist.")
+        self.file_path = path
+        self._initialize()
+
+    def _initialize(self):
+        f = h5py.File(self.file_path, "r")
+        self.data = f[self.KEY]
+        self.shape = self.data.shape
+        print(f"Keys in the file: {list(f.keys())}")
+        print("Data shape:", self.shape)
+        self.num_images = self.shape[0]
+        self.num_bands = self.shape[1]
+
+    def print(self):
+        for i in self.num_images:
+            print(f"Image {i}:")
+            print(self.data[i])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python clean-rates.py <path_to_h5_file>")
+        sys.exit(1)
+    path = sys.argv[1]
+    cleaner = OperaCleaning(path)
+    cleaner.print()
