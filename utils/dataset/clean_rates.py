2c3eb65a8741064d2e5182b989127b0564b3c12f
Almir Neto <almirneto338@gmail.com>
2025-09-08 12:29:49 -0300

    printing file data

--- utils/dataset/clean_rates.py
+++ utils/dataset/clean_rates.py
@@ -15,11 +15,16 @@ class OperaCleaning:
         self.files = Path(self.base_path).rglob(f"*{self.KEY}*")
 
     def clean_file(self, path: str):
-        with h5py.File(path, "r+") as hf:
+        with h5py.File(path, "r") as hf:
             data = hf[self.KEY]
             num_images = data.shape[0]
+            num_bands = data.shape[1]
+            print(
+                f"Cleaning file: {path}, number of images: {num_images}, number of bands: {num_bands}"
+            )
             for i in range(num_images):
-                pass
+                d = data[i]
+                print(d.shape)
                 # data[i][data[i] < 0] = 0
             # hf[self.KEY][:] = data
 
@@ -35,4 +40,4 @@ if __name__ == "__main__":
         sys.exit(1)
     path = sys.argv[1]
     cleaner = OperaCleaning(path)
-    cleaner.print()
+    cleaner.clean()
