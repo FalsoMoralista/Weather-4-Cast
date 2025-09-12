5e556661895cb1fa8d26ee5a7fedc88134080da9
Almir Neto <almirneto338@gmail.com>
2025-09-12 15:32:37 -0300

    fix: removing compiling

--- energy_based_dinov3.py
+++ energy_based_dinov3.py
@@ -261,7 +261,6 @@ def main(args, resume_preempt=False):
     vjepa.patch_embed = nn.Identity()
 
     vjepa = vjepa.to(device)
-    # vjepa = torch.compile(vjepa, mode="reduce-overhead")
 
     total_params = sum(p.numel() for p in vjepa.parameters() if p.requires_grad)
     print(f"V-jepa Total parameters: {total_params / 1.0e9} B")
@@ -292,8 +291,6 @@ def main(args, resume_preempt=False):
         batch_size=batch_size,
         num_frames=4,
     ).to(device)
-
-    model.vision_decoder = torch.compile(model.vision_decoder, mode="reduce-overhead")
 
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Model Total parameters: {total_params / 1.0e9} B")
