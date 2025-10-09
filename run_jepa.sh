b5e0608ed2d1ac94e3f8eaf25fadae65c0d2d815
Almir Neto <almirneto338@gmail.com>
2025-10-09 19:58:45 -0300

    adding main script for jepa version

---
+++ run_jepa.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Check if the YAML filename argument is provided
+if [ -z "$1" ]; then
+  echo "Error: YAML file name is required as an argument."
+  echo "Usage: sbatch -J inat_3 --gpus 1 --mincpus 16 FGDCC_v2.sh <YAML_FILE_NAME>"
+  exit 1
+fi
+
+# Extract the argument
+YAML_FILE_NAME="$1"
+
+#export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
+
+# Run the Python script with the specified YAML file
+python main_jepa.py \
+  --fname configs/"${YAML_FILE_NAME}.yaml" \
+  --devices cuda:0 #cuda:1 cuda:2 cuda:3 cuda:4 cuda:5 cuda:6 cuda:7
