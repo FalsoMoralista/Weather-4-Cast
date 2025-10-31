9550d599857aa151fe4ce623589b18354132dd2b
Almir Neto <almirneto338@gmail.com>
2025-10-30 22:47:34 -0300

    add softmax normalization to input in EMDLoss forward method

--- src/losses/emd.py
+++ src/losses/emd.py
@@ -6,6 +6,7 @@ class EMDLoss(torch.nn.Module):
         super().__init__()
 
     def forward(self, input, target):
+        input = input.softmax(dim=-1)
         input = input.cumsum(dim=-1)
         emd = torch.sum(torch.abs(input - target))
         return emd
